d84c868b620606d6718c4584d1b1e914d0c0f999
Michaël L <michaellaurac@gmail.com>
2022-04-07 23:01:55 +0200

    Defined a basic constructor of the Field class and added a member function to print a given field

--- main.js
+++ main.js
@@ -4,3 +4,31 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+
+  constructor(array) {
+    this._twoDimensionalArray = array;
+  }
+
+  get twoDimensionalArray() {
+    return this._twoDimensionalArray;
+  }
+
+  print() {
+    this._twoDimensionalArray.forEach((line) => {
+      line.forEach((char) => {
+        process.stdout.write(char);
+      })
+      process.stdout.write('\n');
+    })
+  }
+}
+
+const myField = new Field([
+  ['*', '░', 'O'],
+  ['░', 'O', '░'],
+  ['░', '^', '░'],
+]);
+
+myField.print();
